a9973580d882a9129c2c6981a22aeedc4e8fba44
Jorge Flores <ferrufino2007@gmail.com>
2023-02-04 09:56:55 -0600

    connection options

--- database/connectdb.js
+++ database/connectdb.js
@@ -2,9 +2,16 @@ import mongoose from "mongoose";
 import {config} from "dotenv";
 config();
 
+const connectionpOptions = {
+    dbName: 'db_example',
+  //  useNewParser: true,
+//    useUnifiedTopology: true,
+
+  }
+
 try {
     mongoose.set('strictQuery', false);
-    await mongoose.connect(process.env.URI_MONGO);
+    await mongoose.connect(process.env.URI_MONGO,connectionpOptions);
     console.log("😎😎 db conectada");
 } catch (error) {
     console.log("😒😒" + error);
